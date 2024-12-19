b3294f26d8f928b795e0a1e5e90553ebcd3d7726
Yevhen Soldatov <e.soldatov@barva.tech>
2024-12-19 18:22:43 +0200

    feat: add check pull request

---
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, it, jest } from "@jest/globals";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { CourseItem } from "@/features/courses-list/ui/course-item";
+
+describe("course item", () => {
+  it("should call delete callback", async () => {
+    // test here
+    const onDelete = jest.fn();
+    render(
+      <CourseItem
+        course={{ id: "dsf", description: "sdfsd", name: "dsfdsf" }}
+        onDelete={onDelete}
+      />,
+    );
+    await userEvent.click(screen.getByText("Delete"));
+
+    expect(onDelete).toHaveBeenCalled();
+  });
+});
---
+++ tests/courses-list.spec.ts
@@ -0,0 +1,19 @@
+import { test } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("/");
+  // delete all items before starting the test
+  await page.waitForSelector("text=Delete", { timeout: 2000 });
+  const deleteButtons = await page.$$("text=Delete");
+  for (const deleteButton of deleteButtons) {
+    await deleteButton.click();
+  }
+
+  await page.getByPlaceholder("Name...").click();
+  await page.getByPlaceholder("Name...").fill("Test name");
+  await page.getByPlaceholder("Description...").click();
+  await page.getByPlaceholder("Description...").fill("Description text");
+  await page.getByRole("button", { name: "Add" }).click();
+  await page.getByText("Test nameDescription textDelete").click();
+  await page.getByRole("button", { name: "Delete" }).click();
+});
