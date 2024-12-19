02f5be444052b2a743fdd5c45dc3e39050e9b343
Yevhen Soldatov <e.soldatov@barva.tech>
2024-12-19 19:57:06 +0200

    fix: simple playwright

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -2,18 +2,15 @@ import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://test.soldatov.space");
-  // delete all items before starting the test
-  await page.waitForSelector("text=Delete", { timeout: 2000 });
-  const deleteButtons = await page.$$("text=Delete");
-  for (const deleteButton of deleteButtons) {
-    await deleteButton.click();
-  }
-
   await page.getByPlaceholder("Name...").click();
-  await page.getByPlaceholder("Name...").fill("Test name");
+  await page.getByPlaceholder("Name...").fill("1");
   await page.getByPlaceholder("Description...").click();
-  await page.getByPlaceholder("Description...").fill("Description text");
+  await page.getByPlaceholder("Description...").fill("2");
   await page.getByRole("button", { name: "Add" }).click();
-  await page.getByText("Test nameDescription textDelete").click();
-  await page.getByRole("button", { name: "Delete" }).click();
+  await page.getByText("12").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^12Delete$/ })
+    .getByRole("button")
+    .click();
 });
